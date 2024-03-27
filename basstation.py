5043600a1658f75ef74ca7934065807201e5d430
Leon <104960228+noel12sirrom@users.noreply.github.com>
2024-03-27 03:58:02 -0500

    Added charge customer class and basic functionality for serving and adding customers
    The diff includes:
    
    - Addition of the ChargeCustomer class with methods for initializing and displaying customer information
    - Definition of an empty list for storing charge customer objects
    - Addition of a function for adding charge customers with functionality to assign a unique ID, get customer name and number of representatives, and get license plate numbers for each representative
    - Addition of a function for serving customers that calculates the total cost of items purchased
    - Addition of a while loop for the main program with options to serve customers, add charge customers, update charge customers, delete charge customers, make payments, refuel tanks, and generate reports. The loop continues until the user selects "EXIT PROGRAM".

--- basstation.py
+++ basstation.py
@@ -1,4 +1,4 @@
-
+import random
 class ChargeCustomer:
     def  __init__(self, id, name, numOfRep, pref, plateNum=[]):
         self.id = id
@@ -9,6 +9,8 @@ class ChargeCustomer:
     
     def display(self):
         print(f"ID: {self.id}\nName: {self.name}\nNumber of Representatives: {self.numOfRep}\nPreference: {self.pref}\nLicense Plate Numbers: {self.plateNum}")
+        
+chargeCustomers = []
        
 
 inventory = {"E10-87":{"maxCapacity":75708.23,
@@ -18,7 +20,7 @@ inventory = {"E10-87":{"maxCapacity":75708.23,
                       "price":193.60},
             
             "diesel":{"maxCapacity":94635.29,
-                      "price":182.},
+                      "price":182.30},
             
             "5W-30":{"maxCapacity":None,
                      "price":2900.00},
@@ -33,4 +35,47 @@ inventory = {"E10-87":{"maxCapacity":75708.23,
                       "price":2100.00}
             }
 
-date = "01/01/2000"
+todaysDate = "01/01/2000"
+itemsPurchased = ['5W-30']
+
+def addChargeCustomer():
+    customer = ChargeCustomer()
+    
+    if not chargeCustomers:
+        customer.id = 0
+    else:
+        customer.id = chargeCustomers[-1].id +1
+        
+    
+    customer.name = input("Enter Customer name: ")
+    customer.numOfRep = input("Enter the number of representatives: ")
+    
+    for i in range(int(customer.numOfRep)):
+        customer.plateNum.apppend(input(f"Enter the license pla number for Represetative {i}: "))
+    
+    opt = random.randint(1,2)
+    
+        
+     
+
+
+def serveCustomer():
+    gct = 0.16
+    total = 0
+    for i in range(len(itemsPurchased)):
+        if inventory[itemsPurchased[i]]["maxCapacity"]==None:
+            total+= (inventory[itemsPurchased[i]]["price"]*gct) + inventory[itemsPurchased[i]]["price"]
+        else:
+            total += inventory[itemsPurchased[i]]["price"]
+    
+    print(f"total = {total}")
+    
+
+   
+while True:   
+    opt = input("Serve Customer: 1\nAdd Charge Customer: 2\nUpdate Charge Customer: 3\nDelete Charge Customer: 4\nMake payment to charge account: 5\nRefuel tank: 6\nGenerate report: 7\nEXIT PROGRAM: 8\n  Selection: ")
+    if opt == 1:
+        serveCustomer()
+    elif opt == 8:
+        break
+    
