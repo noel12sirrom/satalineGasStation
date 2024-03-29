6ea4cf152cfaa35ff3b4352ede1d239df2a4edb0
Leon <104960228+noel12sirrom@users.noreply.github.com>
2024-03-29 02:06:01 -0500

    refactored code and added new made progress in the serveCustomer()
    -creates validate() which validates Charge customers
    -updated takeOrder() to now be able to take orders from charge customers
    -updated print receipt to now be able to print receipts for Charge customer

--- basstation.py
+++ basstation.py
@@ -9,13 +9,11 @@ class ChargeCustomer:
     
     def display(self):
         print(f"ID: {self.id}\nName: {self.name}\nNumber of Representatives: {self.numOfRep}\nPreference: {self.pref}\nLicense Plate Numbers: {self.plateNum}")
-        
-
-chargeCustomersList = []
-
-chargeCustomers = []
-
-       
+#test variables 
+testChargeCust = ChargeCustomer(1,"gas Pro", 2, None, ["brd232", "ej232"])
+chargeCustomersList = [testChargeCust]
+todaysDate = "01/01/2000"
+itemsPurchased = ['5W-30']       
 
 inventory = {'87':{"maxCapacity":75708.23,
                     "price":184.90},
@@ -41,24 +39,45 @@ inventory = {'87':{"maxCapacity":75708.23,
                       "price":2100.00}
             }
 
-todaysDate = "01/01/2000"
-
 
 def serveCustomer(typeOfCustomer):
+    currentCustomer = None
     change = 0
     amount = 0    
     total = 0
     tax = 0
+    fuelAmt = 0
     itemsPurchased = {}
     paymentType =""
-                 
-    if typeOfCustomer == "COD":
-        takeOrder()
-        CODpayment() 
-        printReceipt()          
+        
+    def validate():
+        givenId = int(input("\nEnter ID: "))
+        for customer in chargeCustomersList:
+            if customer.id == givenId:
+                nonlocal currentCustomer
+                currentCustomer  = customer
+                break
+        else:
+            print("Charge Account Not found.")
+            return False
+            
+        print(customer.name) 
+    
+        #print out license plate under the under the ID
+        print("See List below for plates eligible for fuel")
+        for index, plate in enumerate(currentCustomer.plateNum, start=1):
+            print(f"{index}. {plate}")
+        while True:
+            opt = input("Eligible Plates (Y/N): ").upper()
+            if opt == 'Y' :
+                return True
+            elif opt  == 'N':
+                return False
+            print("Invalid Entry. Please Enter Y or N.")  
         
     def takeOrder():
-        fuelType =(input("Fuel Type: ").upper())
+        nonlocal total, fuelAmt, tax
+        fuelType =(input("\nFuel Type: ").upper())
         fuelAmt = int(input("Enter fuel amount (litres): "))
         itemsPurchased[fuelType] = fuelAmt #adds it itemsPurchased Dictionary
         
@@ -66,11 +85,12 @@ def serveCustomer(typeOfCustomer):
         if needLubricant == "Y":
             lubeType = random.choice(['5W-30', '5W-40', '15W-40', 'SAE-40'])
             lubeAmt = int(input("Enter lubricant amount: "))
+            #adds lubricant type and amount ot list
             itemsPurchased[lubeType] = lubeAmt
             
         for item in itemsPurchased: #item refers to the dictionary key
             if not inventory[item]["maxCapacity"]:
-                total+= inventory[item]["price"]*lubeAmt
+                total += inventory[item]["price"]*lubeAmt
             else:
                 total += inventory[item]["price"]*fuelAmt
             
@@ -82,35 +102,66 @@ def serveCustomer(typeOfCustomer):
         total += tax    
     
     def CODpayment():
-        paymentType = "Cash tendered"
-                   
-        amount = float(input("Enter Cash tendered: "))
-                
+        nonlocal paymentType, change, amount
+        while True:
+            opt = input("Payment type [(1)Cash/(2)Card]: ")
+            if opt == "1":
+                paymentType = "Cash tendered" 
+                if fuelAmt < 2:
+                    while True:
+                        opt = input("The minimum purchase is two litres for cash payments. By more gas? (Y/N): ").upper()
+                        if opt == "Y":
+                            takeOrder()
+                            CODpayment()
+                        elif opt ==  "N":
+                            break
+                        
+                amount = float(input("Enter Cash tendered: "))
+                break
+            elif  opt == "2":
+                paymentType = "Card"
+                amount = total
+                #validate if card amount greater than $1000
+                if amount < 1000:
+                    while True:
+                        opt = input("The minimum amount is $1000 for card transactions.. Would u like to retake order and add more items? (Y/N): ").upper()
+                        if opt == "Y":
+                            takeOrder()
+                            CODpayment()
+                        elif opt ==  "N":
+                            break
+                break
+            
+            print( "Please enter a valid option")
+        
         if amount < total:
             difference = total - amount
-            amount = float(input(f"Needs ${difference} more!\n Enter Cash tendered again: "))
+            amount = float(input(f"Needs ${difference:.2f} more!\n Enter Cash tendered again: "))
                     
-    
-        
         change = amount - total
+        
     def printReceipt():
-        print(f"\n\n\n{todaysDate}\nType of Customer: {typeOfCustomer}\n----")
+        print(f"\n\n{todaysDate}\nType of Customer: {typeOfCustomer}\n----")
         print(*(f"{key}: {value}\n" for key, value in itemsPurchased.items()))
         print(f"----\nGCT = ${tax}\ntotal = ${total}\n{paymentType} = ${amount}\nChange = ${change:.2f}\n\n")
     
     
-    
-
-itemsPurchased = ['5W-30']
-
+    if typeOfCustomer == "charge":
+        if validate():
+            takeOrder()
+            printReceipt() 
+    elif typeOfCustomer == "COD":
+        takeOrder() 
+        CODpayment() 
+        printReceipt()      
 
 def addChargeCustomer():
     customer = ChargeCustomer()
     
-    if not chargeCustomers:
+    if not chargeCustomersList:
         customer.id = 0
     else:
-        customer.id = chargeCustomers[-1].id +1
+        customer.id = chargeCustomersList[-1].id +1
         
     
     customer.name = input("Enter Customer name: ")
@@ -121,45 +172,20 @@ def addChargeCustomer():
     
 
     opt = random.randint(0,1)
-"""
+
 while True:   
-    opt = input("Serve Customer: 1\nAdd Charge Customer: 2\nUpdate Charge Customer: 3\nDelete Charge Customer: 4\nMake payment to charge account: 5\nRefuel tank: 6\nGenerate report: 7\nEXIT PROGRAM: 8\n  Selection: ")
+    opt = input("\nServe Customer: 1\nAdd Charge Customer: 2\nUpdate Charge Customer: 3\nDelete Charge Customer: 4\nMake payment to charge account: 5\nRefuel tank: 6\nGenerate report: 7\nEXIT PROGRAM: 8\n  Selection: ")
     if opt == '1':
-        print("COD: 1\nCharge: 2\n  Selection: ")
-        if selection == '1':
+        opt = input("Customer Type [(1) COD,(2)Charge ]: ")
+        if opt == '1':
             serveCustomer("COD")
-        elif selction == "2":
-                serveCustomer("Charge")
+        elif opt == '2':
+            serveCustomer("charge")
                  
     elif opt == '8':
         break
-""" 
-serveCustomer()
-=======
-    opt = random.randint(1,2)
-    
-        
-     
+ 
 
-
-def serveCustomer():
-    gct = 0.16
-    total = 0
-    for i in range(len(itemsPurchased)):
-        if inventory[itemsPurchased[i]]["maxCapacity"]==None:
-            total+= (inventory[itemsPurchased[i]]["price"]*gct) + inventory[itemsPurchased[i]]["price"]
-        else:
-            total += inventory[itemsPurchased[i]]["price"]
     
-    print(f"total = {total}")
-    
-
-   
-while True:   
-    opt = input("Serve Customer: 1\nAdd Charge Customer: 2\nUpdate Charge Customer: 3\nDelete Charge Customer: 4\nMake payment to charge account: 5\nRefuel tank: 6\nGenerate report: 7\nEXIT PROGRAM: 8\n  Selection: ")
-    if opt == 1:
-        serveCustomer()
-    elif opt == 8:
-        break
     
 
